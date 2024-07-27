f0c92206a50aafc6ec37bc1bcae2f58c81f0f78e
Jannemariesch <janne.scheffler@web.de>
2024-07-27 22:31:32 +0200

    adjusted memory_game

--- memory_game.py
+++ memory_game.py
@@ -2,50 +2,50 @@ import pygame
 import random
 import sys
 
-# Pygame initialisieren
+# starting pygame
 pygame.init()
 
-# Konstanten
+# visualisation
 CARD_SIZE = 80
-GRID_SIZE_X, GRID_SIZE_Y = 3, 2  # 3x2 Raster für 6 Karten
+GRID_SIZE_X, GRID_SIZE_Y = 3, 2  # 3x2 grid for 6 cards
 MARGIN = 10
 FPS = 30
 WIDTH = GRID_SIZE_X * (CARD_SIZE + MARGIN) - MARGIN
 HEIGHT = GRID_SIZE_Y * (CARD_SIZE + MARGIN) - MARGIN
 
-# Pastellfarben für die Karten
 CARD_COLORS = [
-    (173, 216, 230),  # Pastellblau
-    (144, 238, 144),  # Pastellgrün
-    (216, 191, 216),  # Pastelllila
+    (173, 216, 230),  # blue
+    (144, 238, 144),  # green
+    (216, 191, 216),  # purple
 ]
 
-# Bildschirm einrichten
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Memory Game")
 
+
 def create_board():
-    """Erstelle eine Liste von zufällig angeordneten Kartenfarben."""
+    ''' creating a list of randomly distributed cards'''
     cards = CARD_COLORS * (GRID_SIZE_X * GRID_SIZE_Y // len(CARD_COLORS))
     random.shuffle(cards)
     return cards
 
 def draw_board(board, revealed):
-    """Zeichne das gesamte Spielfeld basierend auf der Board- und Offenheitsliste."""
-    screen.fill((0, 0, 0))
-    for y in range(GRID_SIZE_Y):
+    ''' drawing the board of the game '''
+    screen.fill((0, 0, 128))
+    for y in range(GRID_SIZE_Y): # y is rows and x is column on game board, GRID_SIZE for Y and X are amount of rows and columns respectively
         for x in range(GRID_SIZE_X):
-            color = board[y * GRID_SIZE_X + x] if revealed[y][x] else (128, 128, 128)
-            pygame.draw.rect(screen, color, (x * (CARD_SIZE + MARGIN), y * (CARD_SIZE + MARGIN), CARD_SIZE, CARD_SIZE))
+            color = board[y * GRID_SIZE_X + x] if revealed[y][x] else (0, 0, 0) # colors at certain x and y position on game board, gray color on back of card when card is hidden 
+            pygame.draw.rect(screen, color, (x * (CARD_SIZE + MARGIN), y * (CARD_SIZE + MARGIN), CARD_SIZE, CARD_SIZE)) # cards are shown as rectangles 
     pygame.display.flip()
 
 def memory_game():
-    """Führe das Memory-Spiel aus."""
+    ''' running memory game'''
+  
     board = create_board()
     revealed = [[False] * GRID_SIZE_X for _ in range(GRID_SIZE_Y)]
     first_selection, second_selection = None, None
     matches = 0
-    total_pairs = GRID_SIZE_X * GRID_SIZE_Y // 2  # Anzahl der Paare
+    total_pairs = GRID_SIZE_X * GRID_SIZE_Y // 2  # 3 * 2 = 6, number of pairs, needs to be even
     clock = pygame.time.Clock()
 
     while True:
@@ -67,20 +67,20 @@ def memory_game():
 
         draw_board(board, revealed)
 
-        if first_selection and second_selection:
+        if first_selection and second_selection: # checking two selected cards 
             pygame.display.flip()
-            pygame.time.wait(500)  # Warten Sie, um das Paar zu zeigen
-            fx, fy = first_selection
-            sx, sy = second_selection
-            if board[fy * GRID_SIZE_X + fx] == board[sy * GRID_SIZE_X + sx]:
-                matches += 1
-                if matches == total_pairs:
+            pygame.time.wait(500)  # time until cards will either be flipped around again or be marked as a pair (0.5 ms)
+            fx, fy = first_selection # coordinates of first card
+            sx, sy = second_selection # coordinates of second card
+            if board[fy * GRID_SIZE_X + fx] == board[sy * GRID_SIZE_X + sx]: # cards are checked if they have the same color 
+                matches += 1 # if match than match-count increases by 1
+                if matches == total_pairs: # if all matches are found the game will be over 
                     print("You won!")
                     pygame.quit()
                     sys.exit()
             else:
-                revealed[fy][fx] = revealed[sy][sx] = False
-            first_selection, second_selection = None, None
+                revealed[fy][fx] = revealed[sy][sx] = False # if selected cards are not matching they will be flipped again 
+            first_selection, second_selection = None, None 
 
         clock.tick(FPS)
 
